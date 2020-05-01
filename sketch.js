1ad9f6816b49e98eab3295b97e4d530ea5fc4b04
KishaBhatia <64302865+KishaBhatia@users.noreply.github.com>
2020-05-01 16:26:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,9 +18,6 @@ function setup() {
 function draw() {
   background(255,255,255); 
   
-  car.x=World.mouseX; 
-  car.y=World.mouseY; 
-  
   if (wall.x-car.x < (car.width+wall.width)/2)  {
       
     car.velocityX=0;
